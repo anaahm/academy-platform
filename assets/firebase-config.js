e9dcebda2c8b3315fd302758a57f957646efccb7
anaahm <96319035+anaahm@users.noreply.github.com>
2026-09-18 08:47:54 +0300

    Centralize Firebase public app configuration

---
+++ assets/firebase-config.js
@@ -0,0 +1,7 @@
+window.ACADEMY_FIREBASE_CONFIG = Object.freeze({
+  apiKey: "AIzaSyDyaSx72GXPLZqvfinZm6lpWvtLi3jvrR4",
+  authDomain: "talebyg-a2609.firebaseapp.com",
+  databaseURL: "https://talebyg-a2609-default-rtdb.firebaseio.com",
+  projectId: "talebyg-a2609",
+  storageBucket: "talebyg-a2609.firebasestorage.app"
+});
