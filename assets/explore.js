5c1018f6a4feb2ac8a2ac1881030fdf7de661c7a
anaahm <96319035+anaahm@users.noreply.github.com>
2026-09-18 15:07:12 +0300

    Harden Firebase initialization during QA cleanup

--- assets/explore.js
+++ assets/explore.js
@@ -3,7 +3,7 @@
 
 const firebaseConfig = window.ACADEMY_FIREBASE_CONFIG || JSON.parse(localStorage.getItem('academyFirebaseConfig') || 'null');
 if(!firebaseConfig){location.replace('./index.html');return}
-firebase.initializeApp(firebaseConfig);
+if(!firebase.apps.length) firebase.initializeApp(firebaseConfig);
 const db=firebase.database();
 const $=id=>document.getElementById(id), $$=(s,r=document)=>[...r.querySelectorAll(s)];
 
